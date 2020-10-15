88bb42ed1604d454394f57af9bde15f94e2930af
pix <pix666@ya.ru>
2020-10-15 20:44:10 +0400

    fix "{% import %}" tag

--- aioja/compiler.py
+++ aioja/compiler.py
@@ -1,5 +1,5 @@
 from jinja2 import nodes
-from jinja2._compat import string_types
+from jinja2._compat import string_types, imap
 from jinja2.compiler import (
     CodeGenerator,
     CompilerExit,
@@ -115,3 +115,116 @@ class AsyncCodeGenerator(CodeGenerator):
 
         if node.ignore_missing:
             self.outdent()
+
+    def visit_Import(self, node, frame):
+        """
+        FIX:
+        Double `await` for "environment.get_template()._get_default_module_async()"
+        """
+        self.writeline("%s = " % frame.symbols.ref(node.target), node)
+        if frame.toplevel:
+            self.write("context.vars[%r] = " % node.target)
+        if self.environment.is_async:
+            self.write("await (await environment.get_template(")
+            self.visit(node.template, frame)
+            self.write(", %r))." % self.name)
+        else:
+            self.write("environment.get_template(")
+            self.visit(node.template, frame)
+            self.write(", %r)." % self.name)
+        if node.with_context:
+            self.write(
+                "make_module%s(context.get_all(), True, %s)"
+                % (
+                    self.environment.is_async and "_async" or "",
+                    self.dump_local_context(frame),
+                )
+            )
+        elif self.environment.is_async:
+            self.write("_get_default_module_async()")
+        else:
+            self.write("_get_default_module()")
+        if frame.toplevel and not node.target.startswith("_"):
+            self.writeline("context.exported_vars.discard(%r)" % node.target)
+
+    def visit_FromImport(self, node, frame):
+        """
+        FIX:
+        Double `await` for "environment.get_template()._get_default_module_async()"
+        """
+        self.newline(node)
+
+        if self.environment.is_async:
+            self.write("included_template = await (await environment.get_template(")
+            self.visit(node.template, frame)
+            self.write(", %r))." % self.name)
+        else:
+            self.write("included_template = environment.get_template(")
+            self.visit(node.template, frame)
+            self.write(", %r)." % self.name)
+
+        if node.with_context:
+            self.write(
+                "make_module%s(context.get_all(), True, %s)"
+                % (
+                    self.environment.is_async and "_async" or "",
+                    self.dump_local_context(frame),
+                )
+            )
+        elif self.environment.is_async:
+            self.write("_get_default_module_async()")
+        else:
+            self.write("_get_default_module()")
+
+        var_names = []
+        discarded_names = []
+        for name in node.names:
+            if isinstance(name, tuple):
+                name, alias = name
+            else:
+                alias = name
+            self.writeline(
+                "%s = getattr(included_template, "
+                "%r, missing)" % (frame.symbols.ref(alias), name)
+            )
+            self.writeline("if %s is missing:" % frame.symbols.ref(alias))
+            self.indent()
+            self.writeline(
+                "%s = undefined(%r %% "
+                "included_template.__name__, "
+                "name=%r)"
+                % (
+                    frame.symbols.ref(alias),
+                    "the template %%r (imported on %s) does "
+                    "not export the requested name %s"
+                    % (self.position(node), repr(name)),
+                    name,
+                )
+            )
+            self.outdent()
+            if frame.toplevel:
+                var_names.append(alias)
+                if not alias.startswith("_"):
+                    discarded_names.append(alias)
+
+        if var_names:
+            if len(var_names) == 1:
+                name = var_names[0]
+                self.writeline(
+                    "context.vars[%r] = %s" % (name, frame.symbols.ref(name))
+                )
+            else:
+                self.writeline(
+                    "context.vars.update({%s})"
+                    % ", ".join(
+                        "%r: %s" % (name, frame.symbols.ref(name)) for name in var_names
+                    )
+                )
+        if discarded_names:
+            if len(discarded_names) == 1:
+                self.writeline("context.exported_vars.discard(%r)" % discarded_names[0])
+            else:
+                self.writeline(
+                    "context.exported_vars.difference_"
+                    "update((%s))" % ", ".join(imap(repr, discarded_names))
+                )
